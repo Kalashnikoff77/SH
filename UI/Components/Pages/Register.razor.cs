e23ac1c35ebcd075fd8743586b479fdad1b63154
kalashnikoff77 <79265352331@yandex.ru>
2024-07-25 15:18:46 +0300

    Register Step 1 done.

---
+++ UI/Components/Pages/Register.razor.cs
@@ -0,0 +1,135 @@
+﻿using Radzen.Blazor;
+using Radzen;
+using Common.Models;
+using Common.Dto;
+using Common.Dto.Requests;
+using Common.Dto.Responses;
+using Common.Dto.Views;
+using Common.Repository;
+using Microsoft.AspNetCore.Components;
+
+namespace UI.Components.Pages
+{
+    public partial class Register
+    {
+        [Inject] IRepository<GetCountriesModel, GetCountriesRequestDto, GetCountriesResponseDto> _repoGet { get; set; } = null!;
+
+        AccountRegisterModel RegisterModel = new AccountRegisterModel();
+        List<CountriesViewDto> countries { get; set; } = new List<CountriesViewDto>();
+        List<RegionsDto>? regions { get; set; } = new List<RegionsDto>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var apiResponse = await _repoGet.HttpPostAsync(new GetCountriesModel());
+            countries.AddRange(apiResponse.Response.Countries);
+        }
+
+        int countryId {
+            get { return RegisterModel.Country.Id; }
+            set {
+                RegisterModel.Country.Id = value;
+                regions = countries?
+                    .Where(x => x.Id == countryId).FirstOrDefault()?
+                    .Regions?.Select(s => s).ToList();
+            }
+        }
+
+        int regionId {
+            get { return RegisterModel.Country.Region.Id; }
+            set { RegisterModel.Country.Region.Id = value; }
+        }
+
+        private string name;
+        private string savedName;
+
+        private string address;
+        private string savedAddress;
+
+        private string aboutMe;
+        private string savedAboutMe;
+
+        private List<Hobby> hobbies = new List<Hobby>() { new("Games"), new("Sport"), new("Movies"), new("Books"), new("Music") };
+        private IList<Hobby> selectedHobbies = new List<Hobby>();
+        private List<Hobby> savedHobbies = new List<Hobby>();
+
+        private void OnChange()
+        {
+            name = savedName;
+            address = savedAddress;
+            aboutMe = savedAboutMe;
+            selectedHobbies = savedHobbies;
+        }
+
+        private async Task CanChange(StepsCanChangeEventArgs args)
+        {
+            if (args.SelectedIndex == 0 && savedName == name && savedAddress == address)
+            {
+                return;
+            }
+
+            if (args.SelectedIndex == 1 && savedAboutMe == aboutMe)
+            {
+                return;
+            }
+
+            if (args.SelectedIndex == 2 && savedHobbies.SequenceEqual(selectedHobbies))
+            {
+                return;
+            }
+
+            var response = await DialogService.Confirm(
+                "Are you sure you want to continue without saving?",
+                "Confirm",
+                new ConfirmOptions()
+                {
+                    CloseDialogOnEsc = false,
+                    CloseDialogOnOverlayClick = false,
+                    ShowClose = false,
+                    CancelButtonText = "No",
+                    OkButtonText = "Yes",
+                });
+
+            if (response == false)
+            {
+                args.PreventDefault();
+            }
+        }
+
+        private void SaveNameAndAdress()
+        {
+            savedName = name;
+            savedAddress = address;
+        }
+
+        private void SaveAboutMe()
+        {
+            savedAboutMe = aboutMe;
+        }
+
+        private void SaveHobbies()
+        {
+            savedHobbies = selectedHobbies.ToList();
+        }
+
+        private class Hobby
+        {
+            public Hobby(string hobbyName)
+            {
+                HobbyName = hobbyName;
+            }
+
+            public string HobbyName { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                return obj is Hobby hobby && hobby.HobbyName == HobbyName;
+            }
+
+            public override int GetHashCode()
+            {
+                return HobbyName.GetHashCode();
+            }
+        }
+
+    }
+}

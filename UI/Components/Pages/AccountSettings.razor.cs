916e19378651b1645085772b1a2ed369f998a8e1
kalashnikoff77 <79265352331@yandex.ru>
2024-08-05 13:06:21 +0300

    WIP.

--- UI/Components/Pages/AccountSettings.razor.cs
+++ UI/Components/Pages/AccountSettings.razor.cs
@@ -1,13 +1,18 @@
 ﻿using AutoMapper;
+using Common;
 using Common.Dto;
 using Common.Dto.Requests;
 using Common.Dto.Responses;
 using Common.Dto.Views;
+using Common.Enums;
 using Common.Models;
+using Common.Models.SignalR;
 using Common.Models.States;
 using Common.Repository;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using PhotoSauce.MagicScaler;
 using Radzen;
 using Radzen.Blazor;
 using System.Text.Json;
@@ -17,8 +22,10 @@ namespace UI.Components.Pages
 {
     public partial class AccountSettings
     {
-        [Inject] IRepository<AccountUpdateModel, AccountUpdateRequestDto, AccountUpdateResponseDto> _repoUpdate { get; set; } = null!;
+        [Inject] IRepository<AccountUpdateModel, AccountUpdateRequestDto, AccountUpdateResponseDto> _repoAccountUpdate { get; set; } = null!;
         [Inject] IRepository<GetCountriesModel, GetCountriesRequestDto, GetCountriesResponseDto> _repoGetCountries { get; set; } = null!;
+        [Inject] IRepository<UpdatePhotoModel, UpdatePhotoRequestDto, ResponseDtoBase> _repoPhotoUpdate { get; set; } = null!;
+        [Inject] IRepository<UploadPhotoModel, UploadPhotoRequestDto, ResponseDtoBase> _repoPhotoUpload { get; set; } = null!;
         [Inject] IMapper _mapper { get; set; } = null!;
         [Inject] ProtectedLocalStorage _protectedLocalStore { get; set; } = null!;
         [Inject] ProtectedSessionStorage _protectedSessionStore { get; set; } = null!;
@@ -31,6 +38,7 @@ namespace UI.Components.Pages
         AccountUpdateModel UpdatingModel { get; set; } = null!;
 
         int selectedIndex = 0;
+        bool shouldRender = true;
 
         protected override async Task OnInitializedAsync()
         {
@@ -103,5 +111,138 @@ namespace UI.Components.Pages
         }
 
         #endregion
+
+        #region /// ШАГ 3: ФОТО ///
+        async Task AvatarChangedAsync(bool isChecked, AccountsPhotosDto photo)
+        {
+            shouldRender = false;
+
+            // Снимем галку у прежнего аватара
+            var oldAvatar = CurrentState.Account?.Photos?.FirstOrDefault(x => x.IsAvatar == true);
+
+            if (oldAvatar != null)
+            {
+                var oldModel = new UpdatePhotoModel
+                {
+                    Token = CurrentState.Account!.Token,
+                    Guid = oldAvatar.Guid,
+                    IsAvatar = false,
+                    Comment = oldAvatar.Comment,
+                    IsDeleted = false
+                };
+                await _repoPhotoUpdate.HttpPostAsync(oldModel);
+            };
+
+            var model = new UpdatePhotoModel
+            {
+                Token = CurrentState.Account!.Token,
+                Guid = photo.Guid,
+                IsAvatar = isChecked,
+                Comment = photo.Comment,
+                IsDeleted = false
+            };
+            await _repoPhotoUpdate.HttpPostAsync(model);
+
+            await CurrentState.ReloadAccountAsync();
+
+            // Изменим аватары у всех залогиненных пользователей
+            var avatarChangedTriggerModel = new AvatarChangedModel
+            {
+                AccountId = CurrentState.Account.Id,
+                IsAvatar = isChecked,
+                Guid = photo.Guid,
+                Comment = photo.Comment
+            };
+            await CurrentState.SignalRServerAsync(EnumSignalRHandlers.AvatarChangedServer, avatarChangedTriggerModel);
+
+            shouldRender = true;
+        }
+
+        async Task CommentChangedAsync(string NewComment, AccountsPhotosDto photo)
+        {
+            shouldRender = false;
+
+            var model = new UpdatePhotoModel
+            {
+                Token = CurrentState.Account!.Token,
+                Guid = photo.Guid,
+                IsAvatar = photo.IsAvatar,
+                Comment = string.IsNullOrWhiteSpace(NewComment) ? null : NewComment.Trim(),
+                IsDeleted = false
+            };
+            await _repoPhotoUpdate.HttpPostAsync(model);
+
+            await CurrentState.ReloadAccountAsync();
+
+            shouldRender = true;
+        }
+
+        //async void UploadPhotosAsync(InputFileChangeEventArgs e)
+        //{
+        //    var selectedFiles = e.GetMultipleFiles();
+
+        //    var model = new UploadPhotoModel { Token = CurrentState.Account!.Token };
+
+        //    foreach (var file in selectedFiles)
+        //    {
+        //        var extension = new FileInfo(file.Name).Extension;
+
+        //        using (MemoryStream input = new MemoryStream(3500000))
+        //        {
+        //            await file.OpenReadStream(25000000).CopyToAsync(input);
+        //            input.Position = 0;
+
+        //            var photoName = $"{Guid.NewGuid()}{extension}";
+
+        //            using (MemoryStream output = new MemoryStream(500000))
+        //            {
+        //                MagicImageProcessor.ProcessImage(input, output, StaticData.Images[EnumImageSize.s768x1024]);
+        //                await File.WriteAllBytesAsync($"wwwroot/images/AccountsPhotos/temp/{photoName}", output.ToArray());
+        //            }
+
+        //            model.PhotoNames.Add(photoName);
+        //        }
+        //    }
+
+        //    await _repoPhotoUpload.HttpPostAsync(model);
+
+        //    await CurrentState.ReloadAccountAsync();
+
+        //    StateHasChanged(); // Рендеринг нужно вызвать вручную
+        //}
+
+        async Task DeletePhotoAsync(AccountsPhotosDto photo)
+        {
+            shouldRender = false;
+
+            var model = new UpdatePhotoModel
+            {
+                Token = CurrentState.Account!.Token,
+                Guid = photo.Guid,
+                IsAvatar = photo.IsAvatar,
+                Comment = photo.Comment,
+                IsDeleted = true
+            };
+            await _repoPhotoUpdate.HttpPostAsync(model);
+
+            await CurrentState.ReloadAccountAsync();
+
+            // Если удалили аватар, то изменим аватары у всех залогиненных пользователей
+            if (photo.IsAvatar) 
+            {
+                var avatarChangedTriggerModel = new AvatarChangedModel
+                {
+                    AccountId = CurrentState.Account.Id,
+                    IsAvatar = false,
+                    Guid = photo.Guid,
+                    Comment = photo.Comment
+                };
+                await CurrentState.SignalRServerAsync(EnumSignalRHandlers.AvatarChangedServer, avatarChangedTriggerModel);
+            }
+
+            shouldRender = true;
+        }
+        #endregion
+
     }
 }

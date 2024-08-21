54384004e30a4072fccc3822f6fe24f2b86420bb
kalashnikoff77 <79265352331@yandex.ru>
2024-08-21 19:21:57 +0300

    WIP.

--- DataContext/Entities/FeaturesForEvents.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DataContext.Entities
-{
-    public class FeaturesForEvents : EntityBase
-    {
-        public int EventId { get; set; }
-        public int FeatureId { get; set; }
-    }
-}
---
+++ DataContext/Entities/FeaturesForSchedules.cs
@@ -0,0 +1,8 @@
+﻿namespace DataContext.Entities
+{
+    public class FeaturesForSchedules : EntityBase
+    {
+        public int ScheduleId { get; set; }
+        public int FeatureId { get; set; }
+    }
+}

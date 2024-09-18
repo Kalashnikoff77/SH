aa1980f83185fbf2f841dc5e90126e0eab873229
Олег Калашников <okalashnikov@inno.tech>
2024-09-18 12:40:45 +0300

    WIP.

---
+++ UI/Components/Shared/Dialogs/EventCardDialog/Tab_Messages.razor.cs
@@ -0,0 +1,26 @@
+﻿using Common.Dto.Requests;
+using Common.Dto.Responses;
+using Common.Dto.Views;
+using Common.Repository;
+using Microsoft.AspNetCore.Components;
+
+namespace UI.Components.Shared.Dialogs.EventCardDialog
+{
+    public partial class Tab_Messages
+    {
+        [Parameter, EditorRequired] public SchedulesForEventsViewDto ScheduleForEventView { get; set; } = null!;
+        [Inject] IRepository<GetDiscussionsForEventsRequestDto, GetDiscussionsForEventsResponseDto> _repoGetDiscussions { get; set; } = null!;
+
+        List<DiscussionsForEventsViewDto> discussions = null!;
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var response = await _repoGetDiscussions.HttpPostAsync(new GetDiscussionsForEventsRequestDto()
+            {
+                EventId = ScheduleForEventView.EventId
+            });
+
+            discussions = response.Response.Discussions;
+        }
+    }
+}

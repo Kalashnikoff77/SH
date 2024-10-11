2527f611d97d1d29fad09f42ef925a685121fe6e
kalashnikoff77 <79265352331@yandex.ru>
2024-10-11 18:21:31 +0300

    WIP. Разработка формы добавления расписания.

--- UI/Components/Shared/Dialogs/AddEventScheduleDialog.razor.cs
+++ UI/Components/Shared/Dialogs/AddEventScheduleDialog.razor.cs
@@ -8,9 +8,62 @@ namespace UI.Components.Shared.Dialogs
     {
         [CascadingParameter] MudDialogInstance MudDialog { get; set; } = null!;
 
+        SchedulesForEventsDto Schedule { get; set; } = new SchedulesForEventsDto();
+
+        const int maxStartDateDays = 30 * 3;
+        const int maxEndDateDays = 30;
         bool IsFormValid = false;
 
-        SchedulesForEventsDto Schedule { get; set; } = new SchedulesForEventsDto();
+
+        #region /// StartDate ///
+        string? StartDateValidator(DateTime? startDate)
+        {
+            string? errorMessage = null;
+            if (startDate == null)
+                errorMessage = $"Укажите дату начала мероприятия";
+
+            if (startDate.HasValue && (startDate < DateTime.Now || startDate > DateTime.Now.AddDays(maxStartDateDays)))
+                errorMessage = $"Максимальная дата {DateTime.Now.AddDays(maxStartDateDays).ToString("dd.MM.yyyy")}";
+
+            return errorMessage;
+        }
+
+        Color StartDateIconColor = Color.Default;
+        Color StartTimeIconColor = Color.Default;
+
+        DateTime? StartDate
+        {
+            get => Schedule.StartDate == DateTime.MinValue ? null : Schedule.StartDate;
+            set { if (value != null) { Schedule.StartDate = value.Value; } }
+        }
+        TimeSpan? StartTime { get; set; }
+        #endregion
+
+
+        #region /// EndDate ///
+        Color EndDateIconColor = Color.Default;
+        Color EndTimeIconColor = Color.Default;
+        string? EndDateValidator(DateTime? endDate)
+        {
+            string? errorMessage = null;
+            if (endDate == null)
+                errorMessage = $"Укажите дату окончания мероприятия";
+
+            if (endDate.HasValue && (endDate < StartDate || endDate > StartDate!.Value.AddDays(maxEndDateDays)))
+                errorMessage = $"Максимальная дата {StartDate.Value.AddDays(maxEndDateDays).ToString("dd.MM.yyyy")}";
+
+            return errorMessage;
+        }
+
+        DateTime? EndDate
+        {
+            get => Schedule.EndDate == DateTime.MinValue ? null : Schedule.EndDate;
+            set { if (value != null) { Schedule.EndDate = value.Value; } }
+        }
+
+        TimeSpan? EndTime { get; set; }
+        #endregion
+
 
         void Submit() => MudDialog.Close(DialogResult.Ok(Schedule));
 

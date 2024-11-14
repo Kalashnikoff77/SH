82c5c68b2938001f91c9297ca1672ae121344225
Олег Калашников <okalashnikov@inno.tech>
2024-11-14 18:56:19 +0300

    WIP.

--- UI/Components/Pages/Index.razor.cs
+++ UI/Components/Pages/Index.razor.cs
@@ -1,66 +1,45 @@
-﻿using MudBlazor;
-
-namespace UI.Components.Pages
+﻿namespace UI.Components.Pages
 {
     public partial class Index
     {
-        MudDataGrid<Item> dataGrid;
-        string searchString = null!;
-        int counter = 5;
+        IEnumerable<CItems> _selectedItems = new List<CItems>();
+        IEnumerable<CItems> selectedItems { get; set; } = new HashSet<CItems>();
 
-        DateTime[] dates = [DateTime.Parse("13.08.2024")];
-
-        string? DateClass(DateTime date)
+        List<CItems> items = new List<CItems>
         {
-            if (dates.Contains(date))
-                return "event-selected-date";
-            else
-                return null;
-        }
-
-        private async Task<GridData<Item>> ServerReload(GridState<Item> state)
-        {
-            var t1 = state.Page;
-            var t2 = state.PageSize;
-
-            if (!string.IsNullOrWhiteSpace(searchString))
-            {
-                data = data.Where(x => x.Name == searchString).ToList();
-            }
-
-            var items = new GridData<Item>
-            {
-                Items = data.Skip(state.Page * state.PageSize).Take(state.PageSize).ToArray(),
-                TotalItems = data.Count
-            };
-            return items;
-        }
-
-        private Task OnSearch(string text)
-        {
-            searchString = text;
-            return dataGrid.ReloadServerData();
-        }
-
-        void AddItem()
-        {
-            data.Add(new Item { Id = counter++, Name = "Олег" + counter, Date = DateTime.Parse("29/01/1977") });
-            dataGrid.ReloadServerData();
-        }
-
-        List<Item> data = new List<Item>
-        {
-            new Item { Id = 1, Name = "Олег", Date = DateTime.Parse("29/01/1977") },
-            new Item { Id = 2, Name = "Марина", Date = DateTime.Parse("01/07/1969") },
-            new Item { Id = 3, Name = "Сергей", Date = DateTime.Parse("01/10/1969") },
-            new Item { Id = 4, Name = "Татьяна", Date = DateTime.Parse("15/09/1970") },
+            new CItems { Id = 1, Name = "Oleg" },
+            new CItems { Id = 2, Name = "Dima" }
         };
+
+        void OnAdded()
+        {
+            items.Add(new CItems { Id = 3, Name = "Sasha" });
+        }
+
+        void OnDeleted()
+        {
+            ((HashSet<CItems>)selectedItems).Remove(items[0]);
+            items.Remove(items[0]);
+            StateHasChanged();
+            //selectedItems = null;
+        }
+
+        void OnChanged()
+        {
+            items[0].Name = "Oleg_changed";
+        }
+
     }
 
-    public class Item
+    class CItems
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public DateTime Date { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
+
 }

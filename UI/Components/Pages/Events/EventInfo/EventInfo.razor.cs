14a98921383f846fb99f0a175b3c858039213e1c
kalashnikoff77 <79265352331@yandex.ru>
2024-11-25 19:01:32 +0300

    WIP.

--- UI/Components/Pages/Events/EventInfo/EventInfo.razor.cs
+++ UI/Components/Pages/Events/EventInfo/EventInfo.razor.cs
@@ -1,9 +1,12 @@
-﻿using Common.Dto.Requests;
+﻿using Common.Dto;
+using Common.Dto.Requests;
 using Common.Dto.Responses;
 using Common.Dto.Views;
 using Common.Models.States;
 using Common.Repository;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using UI.Components.Dialogs;
 
 namespace UI.Components.Pages.Events.EventInfo
 {
@@ -13,14 +16,60 @@ namespace UI.Components.Pages.Events.EventInfo
         [Parameter, EditorRequired] public int EventId { get; set; }
 
         [Inject] IRepository<GetEventsRequestDto, GetEventsResponseDto> _repoGetEvent { get; set; } = null!;
+        [Inject] IRepository<GetSchedulesDatesRequestDto, GetSchedulesDatesResponseDto> _repoGetSchedulesDates { get; set; } = null!;
+        [Inject] IRepository<GetSchedulesRequestDto, GetSchedulesResponseDto> _repoGetSchedules { get; set; } = null!;
+        [Inject] ShowDialogs ShowDialogs { get; set; } = null!;
 
         EventsViewDto Event { get; set; } = null!;
+        SchedulesForEventsViewDto ScheduleForEventView { get; set; } = null!;
+        SchedulesDatesViewDto? selectedSchedule { get; set; } = null!;
+
+        IEnumerable<SchedulesDatesViewDto> scheduleDates { get; set; } = null!;
+        
+        MudCarousel<PhotosForEventsDto> Carousel = null!;
 
         protected override async Task OnInitializedAsync()
         {
-            var response = await _repoGetEvent.HttpPostAsync(new GetEventsRequestDto { EventId = EventId });
+            var response = await _repoGetEvent.HttpPostAsync(new GetEventsRequestDto { EventId = EventId, IsPhotosIncluded = true });
             if (response.Response.Event != null)
+            {
                 Event = response.Response.Event;
+                scheduleDates = await GetScheduleDates(EventId);
+
+                selectedSchedule = scheduleDates.Where(x => x.StartDate > DateTime.Now).FirstOrDefault();
+                if (selectedSchedule != null)
+                {
+                    ScheduleForEventView = await GetScheduleForEvent(selectedSchedule.Id);
+                }
+                else
+                {
+                    selectedSchedule = scheduleDates.First();
+                    ScheduleForEventView = await GetScheduleForEvent(selectedSchedule.Id);
+                }
+            }
         }
+
+        async Task ScheduleChangedAsync(SchedulesDatesViewDto schedule)
+        {
+            ScheduleForEventView = await GetScheduleForEvent(schedule.Id);
+        }
+
+
+        async Task<SchedulesForEventsViewDto> GetScheduleForEvent(int scheduleId)
+        {
+            var response = await _repoGetSchedules.HttpPostAsync(new GetSchedulesRequestDto { ScheduleId = scheduleId });
+            if (response.Response.Schedule == null)
+                throw new Exception("Не найдено мероприятие!");
+            return response.Response.Schedule;
+        }
+
+        async Task<IEnumerable<SchedulesDatesViewDto>> GetScheduleDates(int eventId)
+        {
+            var responseSchedulesDates = await _repoGetSchedulesDates.HttpPostAsync(new GetSchedulesDatesRequestDto { EventId = eventId });
+            if (responseSchedulesDates.Response.SchedulesDates == null)
+                throw new Exception("Не найдено на одного расписания у мероприятия!");
+            return responseSchedulesDates.Response.SchedulesDates;
+        }
+
     }
 }

d33b918542476a1581fb47acb07379471fac22ea
kalashnikoff77 <79265352331@yandex.ru>
2024-11-27 18:19:23 +0300

    WIP.

---
+++ UI/Components/Pages/Events/EventInfo/Tab_About.razor.cs
@@ -0,0 +1,19 @@
+﻿using Common.Dto.Views;
+using Common.JSProcessor;
+using Microsoft.AspNetCore.Components;
+using UI.Components.Dialogs;
+
+namespace UI.Components.Pages.Events.EventInfo
+{
+    public partial class Tab_About
+    {
+        [Parameter, EditorRequired] public SchedulesForEventsViewDto ScheduleForEventView { get; set; } = null!;
+        [Inject] ShowDialogs ShowDialogs { get; set; } = null!;
+        [Inject] IJSProcessor _JSProcessor { get; set; } = null!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await _JSProcessor.ScrollToElement("top");
+        }
+    }
+}
